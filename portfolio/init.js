23492771e661fc07c6e45daf077225f2001fd2ea
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2023-09-23 21:03:42 +0200

    portfolio update

--- portfolio/init.js
+++ portfolio/init.js
@@ -10,3 +10,34 @@
         }, 150);
     })
 })();
+
+
+function textCorrupt(domElement) {
+    const originalText = domElement.textContent.trim();
+
+    let currentIndex = 0;
+    const corruptText = () => {
+        const textArray = originalText.split('');
+
+        while (currentIndex < textArray.length && textArray[currentIndex] === ' ') {
+            currentIndex++;
+        }
+
+        if (currentIndex >= textArray.length) {
+            clearInterval(corruptInterval);
+            domElement.textContent = originalText;
+        } else {
+            textArray[currentIndex] = getRandomChar();
+            domElement.textContent = textArray.join('');
+            currentIndex++;
+        }
+    };
+
+    const getRandomChar = () => {
+        const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+        const randomIndex = Math.floor(Math.random() * possibleChars.length);
+        return possibleChars.charAt(randomIndex);
+    };
+
+    const corruptInterval = setInterval(corruptText, 100);
+}
